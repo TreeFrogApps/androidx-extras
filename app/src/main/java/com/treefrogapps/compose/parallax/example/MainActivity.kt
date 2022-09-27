e2b21b5f65f59ab173e399105870889eeaa786e5
keenm <treefrogapplications@gmail.com>
2022-09-27 13:35:33 +0100

    add vertical pager into example

--- app/src/main/java/com/treefrogapps/compose/parallax/example/MainActivity.kt
+++ app/src/main/java/com/treefrogapps/compose/parallax/example/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
@@ -13,11 +16,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.treefrogapps.compose.parallax.pager.HorizontalParallaxPageIndicator
-import com.treefrogapps.compose.parallax.pager.HorizontalParallaxPager
-import com.treefrogapps.compose.parallax.pager.ParallaxPage
-import com.treefrogapps.compose.parallax.pager.rememberParallaxPagerState
+import com.treefrogapps.compose.parallax.pager.*
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -40,47 +41,35 @@ class MainActivity : ComponentActivity() {
                     Text(
                         modifier = Modifier.padding(all = 12.dp),
                         text = "Page $pageIdx",
-                        style = MaterialTheme.typography.h4
+                        style = MaterialTheme.typography.h5
                     )
                 },
                 footer = {
                     Text(
                         modifier = Modifier.padding(all = 12.dp),
                         text = "Page Offset ${currentPagePixelOffset().value}",
-                        style = MaterialTheme.typography.h6
+                        style = MaterialTheme.typography.body1
                     )
                 },
                 content = {
-                    Box(
-                        modifier = Modifier
-                            .size(200.dp)
-                            .background(color = Color.Black)
-                    )
-                }).addParallaxLayer { _, _ ->
-                Box(
-                    modifier = Modifier
-                        .size(150.dp)
-                        .background(color = Color.Blue)
-                )
-            }.addParallaxLayer { _, _ ->
-                Box(
-                    modifier = Modifier
-                        .size(100.dp)
-                        .background(color = Color.Red)
-                )
-            }.addParallaxLayer { _, _ ->
-                Box(
-                    modifier = Modifier
-                        .size(50.dp)
-                        .background(color = Color.Green)
-                )
-            }
+                    ParallaxContent(size = 200.dp, color = Color.Blue.copy(alpha = 0.1F))
+                })
+                .addParallaxLayer { _, _ ->
+                    ParallaxContent(size = 150.dp, color = Color.Blue.copy(alpha = 0.2F))
+                }.addParallaxLayer { _, _ ->
+                    ParallaxContent(size = 100.dp, color = Color.Blue.copy(alpha = 0.3F))
+                }.addParallaxLayer { _, _ ->
+                    ParallaxContent(size = 50.dp, color = Color.Blue.copy(alpha = 0.4F))
+                }.addParallaxLayer { _, _ ->
+                    ParallaxContent(size = 25.dp, color = Color.Blue.copy(alpha = 0.5F))
+                }
             (0 until 10).map { page }
         }
 
         MaterialTheme {
             Column {
                 Box(
+                    modifier = Modifier.weight(weight = 0.5F),
                     contentAlignment = Alignment.BottomCenter
                 ) {
                     val state = rememberParallaxPagerState()
@@ -88,6 +77,7 @@ class MainActivity : ComponentActivity() {
                     HorizontalParallaxPager(
                         modifier = Modifier.background(color = color),
                         state = state,
+                        effect = ParallaxEffect.Medium,
                         pages = pages
                     )
                     HorizontalParallaxPageIndicator(
@@ -106,11 +96,49 @@ class MainActivity : ComponentActivity() {
                             ::Triple
                         ).onEach { (page, _, rtOffset) ->
                             color = when (page) {
-                                0    -> Color.Yellow
-                                1    -> Color.Red
-                                2    -> Color.Cyan
-                                3    -> Color.Magenta
-                                4    -> Color.Blue
+                                0 -> Color.Yellow
+                                1 -> Color.Red
+                                2 -> Color.Cyan
+                                3 -> Color.Magenta
+                                4 -> Color.Blue
+                                else -> Color.Yellow
+                            }.copy(alpha = max(a = 0.0F, b = 0.25F - abs(rtOffset) / 2))
+                        }.launchIn(scope = this)
+                    }
+                }
+                Box(
+                    modifier = Modifier.weight(weight = 0.5F),
+                    contentAlignment = Alignment.CenterStart
+                ) {
+                    val state = rememberParallaxPagerState()
+                    var color by remember(state) { mutableStateOf(Color.Yellow.copy(alpha = 0.5F)) }
+                    VerticalParallaxPager(
+                        modifier = Modifier.background(color = color),
+                        state = state,
+                        effect = ParallaxEffect.Medium,
+                        pages = pages
+                    )
+                    VerticalParallaxPageIndicator(
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        state = state,
+                        pageCount = pages.size,
+                        pageIndicatorColor = Color.LightGray,
+                        activePageIndicatorColor = Color.Blue
+                    )
+
+                    LaunchedEffect(key1 = state) {
+                        combine(
+                            snapshotFlow { state.currentPage.value },
+                            snapshotFlow { state.currentPagePixelOffset.value },
+                            snapshotFlow { state.currentPageRatioOffset.value },
+                            ::Triple
+                        ).onEach { (page, _, rtOffset) ->
+                            color = when (page) {
+                                0 -> Color.Yellow
+                                1 -> Color.Red
+                                2 -> Color.Cyan
+                                3 -> Color.Magenta
+                                4 -> Color.Blue
                                 else -> Color.Yellow
                             }.copy(alpha = max(a = 0.0F, b = 0.25F - abs(rtOffset) / 2))
                         }.launchIn(scope = this)
@@ -118,6 +146,15 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    @Composable
+    private fun ParallaxContent(size: Dp, color: Color) {
+        Box(
+            modifier = Modifier
+                .size(size = size)
+                .background(color = color)
+        )
     }
 
     @Composable
