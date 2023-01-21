e5f08f927b4bcec6e749eb03fac0ba02d897bb1e
keenm <treefrogapplications@gmail.com>
2023-01-21 17:17:54 +0000

    add dp extension functions

--- compose-ui/src/main/java/com/treefrogapps/androidx/compose/ui/unit/Dp.kt
+++ compose-ui/src/main/java/com/treefrogapps/androidx/compose/ui/unit/Dp.kt
@@ -1,10 +1,18 @@
 package com.treefrogapps.androidx.compose.ui.unit
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 
-fun Dp.toPaddingValues() : PaddingValues = PaddingValues(all = this)
+fun Dp.toPaddingValues(): PaddingValues = PaddingValues(all = this)
 
-fun Dp.toHorizontalPaddingValues() : PaddingValues = PaddingValues(horizontal = this)
+fun Dp.toHorizontalPaddingValues(): PaddingValues = PaddingValues(horizontal = this)
 
-fun Dp.toVerticalPaddingValues() : PaddingValues = PaddingValues(vertical = this)
+fun Dp.toVerticalPaddingValues(): PaddingValues = PaddingValues(vertical = this)
+
+@Composable
+fun Dp.dpToPx() = with(LocalDensity.current) { this@dpToPx.toPx() }
+
+@Composable
+fun Int.pxToDp() = with(LocalDensity.current) { this@pxToDp.toDp() }
