3fd1519a51aee458c7c80ed6a13e9dc62a922797
keenm <treefrogapplications@gmail.com>
2023-03-11 22:57:37 +0000

    compose material : add range preference

--- compose-material/src/main/java/com/treefrogapps/androidx/compose/material/preference/RangePreference.kt
+++ compose-material/src/main/java/com/treefrogapps/androidx/compose/material/preference/RangePreference.kt
@@ -1,10 +1,16 @@
 package com.treefrogapps.androidx.compose.material.preference
 
 import android.content.res.Configuration
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Card
+import androidx.compose.material.Slider
+import androidx.compose.material.SliderColors
+import androidx.compose.material.SliderDefaults
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -12,9 +18,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -26,21 +34,23 @@ import com.treefrogapps.androidx.compose.material.theme.windowSizeOf
 import kotlinx.coroutines.delay
 
 @Composable
-fun <T : Any> ListPreference(
+fun RangePreference(
     title: String,
     icon: Painter? = null,
     isVisible: Boolean = true,
-    items: List<T>,
-    itemNameProvider: (T) -> String,
-    selectedItem: T?,
-    onPreferenceChange: ((T) -> Unit)?,
+    min : Int,
+    max : Int,
+    current: Int?,
+    onPreferenceChange: ((Int) -> Unit)?,
     enabled: Boolean = true,
     preferenceColors: PreferenceColors = PreferenceDefaults.preferenceColors(),
+    sliderColors: SliderColors = SliderDefaults.colors(),
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
 ) {
     var isDialogOpen by rememberSaveable { mutableStateOf(value = false) }
     CorePreference(
         title = title,
-        summary = selectedItem?.let(itemNameProvider::invoke),
+        summary = current?.toString(),
         icon = icon,
         isVisible = isVisible,
         enabled = enabled,
@@ -49,21 +59,25 @@ fun <T : Any> ListPreference(
         innerContent = { })
 
     if (isDialogOpen) {
-        ListPreferenceDialog(
-            items = items,
-            itemNameProvider = itemNameProvider,
-            selectedItem = selectedItem,
+        RangePreferenceDialog(
+            min = min,
+            max = max,
+            current = current,
             onPreferenceChange = onPreferenceChange,
-            onDismiss = { isDialogOpen = false })
+            onDismiss = { isDialogOpen = false },
+            sliderColors = sliderColors,
+            interactionSource = interactionSource)
     }
 }
 
 @Composable
-private fun <T : Any> ListPreferenceDialog(
-    items: List<T>,
-    itemNameProvider: (T) -> String,
-    selectedItem: T?,
-    onPreferenceChange: ((T) -> Unit)?,
+private fun RangePreferenceDialog(
+    min : Int,
+    max : Int,
+    current: Int?,
+    sliderColors: SliderColors,
+    interactionSource: MutableInteractionSource,
+    onPreferenceChange: ((Int) -> Unit)?,
     onDismiss: () -> Unit
 ) {
     Dialog(
@@ -73,18 +87,19 @@ private fun <T : Any> ListPreferenceDialog(
             Column(
                 modifier = Modifier
                     .padding(all = Theme.dimens.spacing.normal)
-                    .defaultMinSize(minWidth = 240.dp)
+                    .defaultMinSize(minWidth = 240.dp),
+                horizontalAlignment = Alignment.End
             ) {
-                items.forEach { item ->
-                    val name = remember { itemNameProvider(item) }
-
-                    TextRadioButton(
-                        text = name,
-                        selected = item == selectedItem,
-                        onClick = {
-                            onPreferenceChange?.invoke(item)
-                            onDismiss()
-                        })
+                Slider(
+                    value = current?.toFloat() ?: min.toFloat(),
+                    onValueChange = { value -> onPreferenceChange?.invoke(value.toInt())},
+                    steps = (max - min).coerceAtLeast(minimumValue = 0),
+                    colors = sliderColors,
+                    interactionSource = interactionSource)
+                Button(
+                    onClick = onDismiss
+                ) {
+                    Text(text = stringResource(id = R.string.done))
                 }
             }
         }
@@ -96,46 +111,37 @@ private fun <T : Any> ListPreferenceDialog(
     showSystemUi = true,
     uiMode = Configuration.UI_MODE_NIGHT_NO)
 @Composable
-private fun ListPreferencePreview() {
+private fun RangePreferencePreview() {
     MaterialThemeExtended(windowSize = windowSizeOf()) {
         var isVisible by remember { mutableStateOf(true) }
-        var selectedItemA by remember { mutableStateOf<String?>(null) }
-        var selectedItemB by remember { mutableStateOf<String?>("Item one") }
-        var selectedItemC by remember { mutableStateOf<String?>("Item three") }
+        var selectedItemA by remember { mutableStateOf<Int?>(null) }
+        var selectedItemB by remember { mutableStateOf<Int?>(50) }
+        var selectedItemC by remember { mutableStateOf<Int?>(75) }
         PreferenceGroup(
-            title = "List Group"
+            title = "Range Group"
         ) {
-            ListPreference(
-                title = "List preference field title",
+            RangePreference(
+                title = "Range preference field title",
                 icon = painterResource(id = R.drawable.ic_confirmation_badge),
-                itemNameProvider = String::toString,
-                items = listOf(
-                    "Item one",
-                    "Item two",
-                    "Item three"),
-                selectedItem = selectedItemA,
+                min = 0,
+                max = 100,
+                current = selectedItemA,
                 enabled = true,
                 onPreferenceChange = { selected -> selectedItemA = selected })
-            ListPreference(
-                title = "List preference field title",
+            RangePreference(
+                title = "Range preference field title",
                 icon = painterResource(id = R.drawable.ic_confirmation_badge),
-                itemNameProvider = String::toString,
-                items = listOf(
-                    "Item one",
-                    "Item two",
-                    "Item three"),
-                selectedItem = selectedItemB,
+                min = 0,
+                max = 100,
+                current = selectedItemB,
                 enabled = false,
                 onPreferenceChange = { selected -> selectedItemB = selected })
-            ListPreference(
-                title = "List preference field title",
+            RangePreference(
+                title = "Range preference field title",
                 icon = painterResource(id = R.drawable.ic_confirmation_badge),
-                itemNameProvider = String::toString,
-                items = listOf(
-                    "Item one",
-                    "Item two",
-                    "Item three"),
-                selectedItem = selectedItemC,
+                min = 0,
+                max = 100,
+                current = selectedItemC,
                 enabled = true,
                 isVisible = isVisible,
                 onPreferenceChange = { selected -> selectedItemC = selected })
@@ -154,46 +160,37 @@ private fun ListPreferencePreview() {
     showSystemUi = true,
     uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
-private fun ListPreferenceDarkPreview() {
+private fun RangePreferenceDarkPreview() {
     MaterialThemeExtended(windowSize = windowSizeOf()) {
         var isVisible by remember { mutableStateOf(true) }
-        var selectedItemA by remember { mutableStateOf<String?>(null) }
-        var selectedItemB by remember { mutableStateOf<String?>("Item one") }
-        var selectedItemC by remember { mutableStateOf<String?>("Item three") }
+        var selectedItemA by remember { mutableStateOf<Int?>(null) }
+        var selectedItemB by remember { mutableStateOf<Int?>(50) }
+        var selectedItemC by remember { mutableStateOf<Int?>(75) }
         PreferenceGroup(
-            title = "List Group"
+            title = "Range Group"
         ) {
-            ListPreference(
-                title = "List preference field title",
+            RangePreference(
+                title = "Range preference field title",
                 icon = painterResource(id = R.drawable.ic_confirmation_badge),
-                itemNameProvider = String::toString,
-                items = listOf(
-                    "Item one",
-                    "Item two",
-                    "Item three"),
-                selectedItem = selectedItemA,
+                min = 0,
+                max = 100,
+                current = selectedItemA,
                 enabled = true,
                 onPreferenceChange = { selected -> selectedItemA = selected })
-            ListPreference(
-                title = "List preference field title",
+            RangePreference(
+                title = "Range preference field title",
                 icon = painterResource(id = R.drawable.ic_confirmation_badge),
-                itemNameProvider = String::toString,
-                items = listOf(
-                    "Item one",
-                    "Item two",
-                    "Item three"),
-                selectedItem = selectedItemB,
+                min = 0,
+                max = 100,
+                current = selectedItemB,
                 enabled = false,
                 onPreferenceChange = { selected -> selectedItemB = selected })
-            ListPreference(
-                title = "List preference field title",
+            RangePreference(
+                title = "Range preference field title",
                 icon = painterResource(id = R.drawable.ic_confirmation_badge),
-                itemNameProvider = String::toString,
-                items = listOf(
-                    "Item one",
-                    "Item two",
-                    "Item three"),
-                selectedItem = selectedItemC,
+                min = 0,
+                max = 100,
+                current = selectedItemC,
                 enabled = true,
                 isVisible = isVisible,
                 onPreferenceChange = { selected -> selectedItemC = selected })
