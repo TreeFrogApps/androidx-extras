2fc844f3c085cd420ed52356737ac2bca9ea6513
keenm <treefrogapplications@gmail.com>
2023-11-25 23:07:04 +0000

    add LocalWindow through a CompositionLocalProvider

--- compose-activity/src/main/java/com/treefrogapps/androidx/compose/activity/Activity.kt
+++ compose-activity/src/main/java/com/treefrogapps/androidx/compose/activity/Activity.kt
@@ -1,9 +1,12 @@
 package com.treefrogapps.androidx.compose.activity
 
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -18,15 +21,30 @@ import kotlinx.coroutines.launch
  *
  * This is a platform bug, however needs to be accounted for in apps that intend to target
  * these devices.
+ *
+ * This implementation also adds [LocalWindow] as a [CompositionLocalProvider] which can then be
+ * referenced by  the client.
  */
 fun ComponentActivity.setContent(
     parent: CompositionContext? = null,
     content: @Composable () -> Unit
 ) {
-    setContent(parent = parent, content = content)
+    setContent(
+        parent = parent,
+        content = {
+            CompositionLocalProvider(
+                values = arrayOf(LocalWindow provides window),
+                content = content
+            )
+        })
 
     lifecycleScope.launch {
         delay(timeMillis = 30)
         window.setBackgroundDrawableResource(android.R.color.transparent)
     }
+}
+
+
+val LocalWindow = staticCompositionLocalOf<Window> {
+    error("CompositionLocal LocalView not present")
 }
