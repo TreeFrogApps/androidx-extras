88ee1f73737198fc59018070c6c1e5cbd7f86063
keenm <treefrogapplications@gmail.com>
2023-11-26 11:51:07 +0000

    tweak implementation and add documentation for color at elevation

--- compose-material/src/main/java/com/treefrogapps/androidx/compose/material/Surface.kt
+++ compose-material/src/main/java/com/treefrogapps/androidx/compose/material/Surface.kt
@@ -1,19 +1,38 @@
 package com.treefrogapps.androidx.compose.material
 
-import androidx.compose.material.ElevationOverlay
+import androidx.compose.material.LocalElevationOverlay
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
+
+/**
+ * Calculate the surface color at [elevation].
+ *
+ * This will return the elevated surface color if [LocalElevationOverlay] is not null, otherwise
+ * this wil return the normal [MaterialTheme.colors] surface color.
+ */
 @Composable
 private fun surfaceColorAtElevation(
-    color: Color,
-    elevationOverlay: ElevationOverlay?,
-    absoluteElevation: Dp = 0.dp
+    elevation: Dp = 0.dp
 ): Color =
-    elevationOverlay
-        ?.takeIf { color == MaterialTheme.colors.surface }
-        ?.apply(color = color, absoluteElevation)
+    LocalElevationOverlay.current
+        ?.apply(color = MaterialTheme.colors.surface, elevation)
+        ?: MaterialTheme.colors.surface
+
+/**
+ * Calculate the color at [elevation].
+ *
+ * This will return the elevated [color] if [LocalElevationOverlay] is not null, otherwise
+ * this wil return [color].
+ */
+@Composable
+private fun colorAtElevation(
+    color: Color,
+    elevation: Dp = 0.dp
+): Color =
+    LocalElevationOverlay.current
+        ?.apply(color = color, elevation)
         ?: color
