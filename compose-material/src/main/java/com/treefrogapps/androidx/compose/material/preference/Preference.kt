da20de061af7003ac5a271676a494b3e79dc28d9
keenm <treefrogapplications@gmail.com>
2024-03-13 21:31:23 +0000

    add preference spacing and bump material lib version

--- compose-material/src/main/java/com/treefrogapps/androidx/compose/material/preference/Preference.kt
+++ compose-material/src/main/java/com/treefrogapps/androidx/compose/material/preference/Preference.kt
@@ -11,9 +11,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -126,6 +128,7 @@ fun CorePreference(
                         )
                     }
                 }
+                Spacer(modifier = Modifier.width(width = Theme.dimens.spacing.normal))
                 innerContent()
             }
             bottomContent()
