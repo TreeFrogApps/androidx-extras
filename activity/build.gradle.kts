0911169f3c884b0417907e1ca9c6f4d57ac46449
keenm <treefrogapplications@gmail.com>
2024-09-24 23:39:11 +0100

    bump library versions

--- activity/build.gradle.kts
+++ activity/build.gradle.kts
@@ -57,7 +57,7 @@ publishing {
         register<MavenPublication>(name = "release") {
             groupId = "com.treefrogapps.androidx.activity"
             artifactId = "activity"
-            version = "1.5.6"
+            version = "1.5.7"
 
             afterEvaluate {
                 from(components["release"])
