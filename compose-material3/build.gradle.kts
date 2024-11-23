1e70555998c76442373584ddc65e2f17a2b49284
keenm <treefrogapplications@gmail.com>
2024-11-23 13:40:46 +0000

    bump libs.versions.toml and bump library versions

--- compose-material3/build.gradle.kts
+++ compose-material3/build.gradle.kts
@@ -87,7 +87,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.material3)
-    implementation(project(":compose-ui"))
+    implementation(projects.composeUi)
 
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
