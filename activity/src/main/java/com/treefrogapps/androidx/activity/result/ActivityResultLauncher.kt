cce1dffcfbde90d98de4cb589df0fb6cf8a6e036
keenm <treefrogapplications@gmail.com>
2024-12-23 20:48:49 +0000

    fix platform function signature clash

--- activity/src/main/java/com/treefrogapps/androidx/activity/result/ActivityResultLauncher.kt
+++ activity/src/main/java/com/treefrogapps/androidx/activity/result/ActivityResultLauncher.kt
@@ -1,7 +1,35 @@
 package com.treefrogapps.androidx.activity.result
 
+import android.content.ActivityNotFoundException
 import androidx.activity.result.ActivityResultLauncher
 import androidx.core.app.ActivityOptionsCompat
 
+inline fun ActivityResultLauncher<Unit>.launch(
+    options: ActivityOptionsCompat? = null,
+    onError: () -> Unit = {}
+) = try {
+    launch(input = Unit, options = options)
+} catch (e: ActivityNotFoundException) {
+    onError()
+}
 
-fun ActivityResultLauncher<Unit>.launch(options: ActivityOptionsCompat? = null) = launch(input = Unit, options = options)
+inline fun <I : Any?> ActivityResultLauncher<I?>.launchNullable(
+    input: I? = null,
+    options: ActivityOptionsCompat? = null,
+    onError: () -> Unit = {}
+) = try {
+    launch(input = input, options = options)
+} catch (e: ActivityNotFoundException) {
+    onError()
+}
+
+inline fun <I : Any> ActivityResultLauncher<I>.launchNotNull(
+    input: I,
+    options: ActivityOptionsCompat? = null,
+    onError: () -> Unit = {}
+) = try {
+    launch(input = input, options = options)
+} catch (e: ActivityNotFoundException) {
+    onError()
+}
+
