c74a139dee0793e2e17583df17af96ca5b79a546
keenm <treefrogapplications@gmail.com>
2025-02-27 10:44:42 +0000

    minor tweak to abstract composable and bum lib

--- compose-activity/build.gradle.kts
+++ compose-activity/build.gradle.kts
@@ -63,7 +63,7 @@ publishing {
         register<MavenPublication>(name = "release") {
             groupId = "com.treefrogapps.androidx.compose"
             artifactId = "compose-activity"
-            version = "1.11.5"
+            version = "1.11.6"
 
             afterEvaluate {
                 from(components["release"])
